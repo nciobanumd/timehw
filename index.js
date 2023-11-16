d725ce1c0dff5233095c1952a93e9b4d5c4c7687
Nadejda Ciobanu <nciobanumd@gmail.com>
2023-11-16 20:50:59 +0200

    adugarea tema cu ora

---
+++ index.js
@@ -0,0 +1,33 @@
+const newtime = document.getElementById('timer')
+let curentTime = Date.now()
+let curentDate = new Date(curentTime)
+let intervalTime
+
+const startCount = () => {
+    
+    let hour = curentDate.getHours();
+    let minute = curentDate.getMinutes();
+    let second = curentDate.getSeconds();
+    
+    intervalTime = setInterval(() => {
+        newtime.innerHTML =
+        (hour ? hour + ':' : '') + 
+        (minute < 10 ? '0' + minute : minute) + ':' +
+        (second < 10 ? '0' + second : second) 
+    
+        second++
+
+        if(second === 60) {
+            minute ++
+            second = 0
+        }
+
+        if(minute === 60) {
+            hour++
+            minute = 0
+        }
+
+    }, 1000)
+
+}
+
